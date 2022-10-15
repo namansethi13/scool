17b4e32b19a0ba237ea2cea0333d99703535edbb
Cheshta Batra <cheshtaabatra1828@gmail.com>
2022-10-15 20:28:09 +0530

    spelling

--- classroom/models.py
+++ classroom/models.py
@@ -1,19 +1,31 @@
 from tkinter import TRUE
 from django.db import models
 
-# Create your models here.
+
+
+
+#create user model here 
+
+
+
+
 
 
 
 #Creating classroom model 
 class classroom(models.Model):
     #<--uncomment when user model is created-->
-    #t_id=models.ForeignKey(user,on_delete=models.cascade)
-    c_id=models.AutoField(primary_key=TRUE)
+    #t_id=models.ForeignKey(user,on_delete=models.CASCADE)
+    c_id=models.AutoField(primary_key=True)
     subject=models.CharField(max_length=100)
     ilink=models.CharField(max_length=5000)
     cname=models.CharField(max_length=500)
-    about=models.CharField(max_length=5000)
+    about=models.TextField()
+    
+
+
+
+#create assignment model here 
 
 
 
